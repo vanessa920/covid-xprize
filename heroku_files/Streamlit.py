4a67491308bd0a9574b86cfbbeb8109e6d6e9c28
Ridwan <ridwan102@gmail.com>
2021-02-09 23:35:08 -0500

    Changed writing

--- heroku_files/Streamlit.py
+++ heroku_files/Streamlit.py
@@ -46,8 +46,8 @@ country_data = df[df['CountryName'] == select]
 
 st.sidebar.markdown("#### Please select Intervention Stringency Level")
 stringency = st.sidebar.slider('', 0, 9)
-# prescribe_df = pd.read_csv("heroku_files/all_2021q1_test_task.csv")
-prescribe_df = pd.read_csv("all_2021q1_test_task.csv")
+prescribe_df = pd.read_csv("heroku_files/all_2021q1_test_task.csv")
+# prescribe_df = pd.read_csv("all_2021q1_test_task.csv")
 prescribe_df = prescribe_df[prescribe_df['CountryName'] == select] #select the country
 prescribe_df = prescribe_df[pd.to_datetime(prescribe_df['Date']) >= datetime.datetime.today()] #select today and future dates
 prescribe_df = prescribe_df[prescribe_df['PrescriptionIndex'] == stringency] #select the relevant prescription index
